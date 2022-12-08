116153356371a8ab9359df4270905aaf8919f956
alelin4 <87665597+alelin4@users.noreply.github.com>
2022-12-08 18:50:38 +0100

    Create Cart.php

---
+++ Cart.php
@@ -0,0 +1,61 @@
+<?php
+
+class Cart
+{
+    private array $items = [];
+
+    //TODO Skriv getter för items
+ public function getItems(){
+
+    return $this ->items;
+ }
+    /*
+     Skall lägga till en produkt i kundvagnen genom att
+     skapa ett nytt cartItem och lägga till i $items array.
+     Metoden skall returnera detta cartItem.
+
+     VG: Om produkten redan finns i kundvagnen
+     skall istället quantity på cartitem ökas.
+     */
+    public function addProduct($product)
+    {
+        $cartItem = new CartItem($product,1);
+
+        $this -> items[$product->getId()]=$cartItem;
+
+        return $cartItem;
+    }
+
+
+    //Skall ta bort en produkt ur kundvagnen (använd unset())
+    public function removeProduct($product)
+    {
+     unset($this->items[$product->getId()]);
+    }
+
+    //Skall returnera totala antalet produkter i kundvagnen
+    //OBS: Ej antalet unika produkter
+    public function getTotalQuantity()
+    {
+    
+        foreach($this->items as $item)
+        {
+        $totalQuantity+=$item->getQuantity();
+
+        };
+
+        return $totalQuantity;
+    
+    }
+
+    //Skall räkna ihop totalsumman för alla produkter i kundvagnen
+    //VG: Tänk på att ett cartitem kan ha olika quantity
+    public function getTotalSum()
+    {
+    $total=0;
+    foreach ($this->items as $item){
+        $total+=$item->getQuantity()* $item->getProduct()->getPrice();
+    }
+    return $total;
+    }
+}
